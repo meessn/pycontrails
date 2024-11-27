dff41e4204e0e2e70f863729fb77b1dd36f595a4
Mees Snoek <meessnoek@gmail.com>
2024-11-27 15:52:24 +0100

    some erased commments

--- emission_index.py
+++ emission_index.py
@@ -488,99 +488,99 @@ def meem_nvpm_5_point(altitude, mach, altitude_cruise, flight_phase, saf):
         return None, None
     return None, None
 
-def p3t3_meem_nvpm(altitude, air_temperature, air_pressure , mach, PT3_inflight, TT3_inflight, P3_inflight, T3_inflight, eta_comp, saf):
-    """
-        meem with actual values from GSP12
-
-        Args:
-
-
-
-
-        Returns:
-
-        """
-
-
-    # first convert altitude from m to ft, as this is used in the paper too
-    altitude_ft = altitude * constants.m_to_ft
-    # altitude_cruise_ft = altitude_cruise * constants.m_to_ft
-    T_sls = 288.15 #K
-    P_sls = 101325 #Pa
-    operating_pr_icao = 31.7
-    rated_thrust_icao = 120.4
-
-    if altitude_ft > 3000:
-        # """STEP 1"""
-        # if altitude_ft < 36089:
-        #     T_alt = T_sls*(1-(altitude_ft/145442))
-        #     P_alt = P_sls*(1-(altitude_ft/145442))**5.255876
-        # else:
-        #     T_alt = T_sls*0.751865
-        #     P_alt = P_sls*0.223361*np.exp(-1*((altitude_ft-36089)/20806))
-
-        T_alt = air_temperature
-        P_alt = air_pressure
-        # print('T_alt', T_alt)
-        # print('P_alt', P_alt)
-        #total properties
-        Tt_alt = T_alt*(1 + ((constants.kappa-1)/2)*(mach**2))
-        Pt_alt = P_alt*(1 + ((constants.kappa-1)/2)*(mach**2))**(constants.kappa/(constants.kappa-1))
-        # print('Tt_alt', Tt_alt)
-        # print('Pt_alt', Pt_alt)
-        # Determine eta_comp and pressure_coef dependent on flight stage:
-        # CLIMB
-        # if flight_phase == 'climb':
-        #     eta_comp = 0.88
-        #     pressure_coef = 0.85 + (1.15-0.85)*((altitude_ft-3000)/(altitude_cruise_ft-3000))
-        # elif flight_phase == 'cruise':
-        #     eta_comp = 0.88
-        #     pressure_coef = 0.95
-        # elif flight_phase == 'descent':
-        #     eta_comp = 0.70
-        #     pressure_coef = 0.12
-        # else:
-        #     return None, None
-        # print('eta_comp', eta_comp)
-        # print('pressure coef', pressure_coef)
-        P3_alt = P3_inflight
-        T3_alt = T3_inflight
-        # print('T3_alt', T3_alt)
-        # print('P3_alt', P3_alt)
-        """STEP 2"""
-        T3_gr = T3_alt
-        P3_gr = ((1 + eta_comp*((T3_gr/T_sls)-1))**(constants.kappa/(constants.kappa-1)))*P_sls
-        # print('T3_gr', T3_gr)
-        # print('P3_gr', P3_gr)
-        F_gr_F_rated = ((P3_gr/P_sls)-1)/(operating_pr_icao-1)
-        # print('F_gr_F_rated', F_gr_F_rated)
-        """STEP 3"""
-        #4 point interpolation method because for EI_mass the
-        # max is at T/O condition and EI_number is also very close to T/O. so 57.5 or 92.5 will be too far off
-        if saf == False:
-            EI_mass_icao_sl = [7.8, 0.6, 26.3, 36.3]
-            EI_number_icao_sl = [5.78e15, 3.85e14, 1.60e15, 1.45e15]
-
-        thrust_setting_icao = [0.07, 0.3, 0.85, 1]
-
-        # Perform interpolation directly and handles the values <0.07 and >1 correctly
-        EI_mass_interpolated_gr = np.interp(F_gr_F_rated, thrust_setting_icao, EI_mass_icao_sl)
-        EI_number_interpolated_gr = np.interp(F_gr_F_rated, thrust_setting_icao, EI_number_icao_sl)
-        # print('EI_mass_interpolated_gr',EI_mass_interpolated_gr )
-        # print('EI_number_interpolated_gr', EI_number_interpolated_gr)
-
-        """STEP 4"""
-        EI_mass_alt = EI_mass_interpolated_gr*((P3_alt/P3_gr)**1.35)*1.1**2.5
-        EI_number_alt = EI_mass_alt*(EI_number_interpolated_gr/EI_mass_interpolated_gr)
-        # print('meem results')
-        # print(EI_mass_alt)
-        # print(EI_number_alt)
-
-        return EI_mass_alt, EI_number_alt
-
-    else:
-        return None, None
-    return None, None
+# def p3t3_meem_nvpm(altitude, air_temperature, air_pressure , mach, PT3_inflight, TT3_inflight, P3_inflight, T3_inflight, eta_comp, saf):
+#     """
+#         meem with actual values from GSP12
+#
+#         Args:
+#
+#
+#
+#
+#         Returns:
+#
+#         """
+#
+#
+#     # first convert altitude from m to ft, as this is used in the paper too
+#     altitude_ft = altitude * constants.m_to_ft
+#     # altitude_cruise_ft = altitude_cruise * constants.m_to_ft
+#     T_sls = 288.15 #K
+#     P_sls = 101325 #Pa
+#     operating_pr_icao = 31.7
+#     rated_thrust_icao = 120.4
+#
+#     if altitude_ft > 3000:
+#         # """STEP 1"""
+#         # if altitude_ft < 36089:
+#         #     T_alt = T_sls*(1-(altitude_ft/145442))
+#         #     P_alt = P_sls*(1-(altitude_ft/145442))**5.255876
+#         # else:
+#         #     T_alt = T_sls*0.751865
+#         #     P_alt = P_sls*0.223361*np.exp(-1*((altitude_ft-36089)/20806))
+#
+#         T_alt = air_temperature
+#         P_alt = air_pressure
+#         # print('T_alt', T_alt)
+#         # print('P_alt', P_alt)
+#         #total properties
+#         Tt_alt = T_alt*(1 + ((constants.kappa-1)/2)*(mach**2))
+#         Pt_alt = P_alt*(1 + ((constants.kappa-1)/2)*(mach**2))**(constants.kappa/(constants.kappa-1))
+#         # print('Tt_alt', Tt_alt)
+#         # print('Pt_alt', Pt_alt)
+#         # Determine eta_comp and pressure_coef dependent on flight stage:
+#         # CLIMB
+#         # if flight_phase == 'climb':
+#         #     eta_comp = 0.88
+#         #     pressure_coef = 0.85 + (1.15-0.85)*((altitude_ft-3000)/(altitude_cruise_ft-3000))
+#         # elif flight_phase == 'cruise':
+#         #     eta_comp = 0.88
+#         #     pressure_coef = 0.95
+#         # elif flight_phase == 'descent':
+#         #     eta_comp = 0.70
+#         #     pressure_coef = 0.12
+#         # else:
+#         #     return None, None
+#         # print('eta_comp', eta_comp)
+#         # print('pressure coef', pressure_coef)
+#         P3_alt = P3_inflight
+#         T3_alt = T3_inflight
+#         # print('T3_alt', T3_alt)
+#         # print('P3_alt', P3_alt)
+#         """STEP 2"""
+#         T3_gr = T3_alt
+#         P3_gr = ((1 + eta_comp*((T3_gr/T_sls)-1))**(constants.kappa/(constants.kappa-1)))*P_sls
+#         # print('T3_gr', T3_gr)
+#         # print('P3_gr', P3_gr)
+#         F_gr_F_rated = ((P3_gr/P_sls)-1)/(operating_pr_icao-1)
+#         # print('F_gr_F_rated', F_gr_F_rated)
+#         """STEP 3"""
+#         #4 point interpolation method because for EI_mass the
+#         # max is at T/O condition and EI_number is also very close to T/O. so 57.5 or 92.5 will be too far off
+#         if saf == False:
+#             EI_mass_icao_sl = [7.8, 0.6, 26.3, 36.3]
+#             EI_number_icao_sl = [5.78e15, 3.85e14, 1.60e15, 1.45e15]
+#
+#         thrust_setting_icao = [0.07, 0.3, 0.85, 1]
+#
+#         # Perform interpolation directly and handles the values <0.07 and >1 correctly
+#         EI_mass_interpolated_gr = np.interp(F_gr_F_rated, thrust_setting_icao, EI_mass_icao_sl)
+#         EI_number_interpolated_gr = np.interp(F_gr_F_rated, thrust_setting_icao, EI_number_icao_sl)
+#         # print('EI_mass_interpolated_gr',EI_mass_interpolated_gr )
+#         # print('EI_number_interpolated_gr', EI_number_interpolated_gr)
+#
+#         """STEP 4"""
+#         EI_mass_alt = EI_mass_interpolated_gr*((P3_alt/P3_gr)**1.35)*1.1**2.5
+#         EI_number_alt = EI_mass_alt*(EI_number_interpolated_gr/EI_mass_interpolated_gr)
+#         # print('meem results')
+#         # print(EI_mass_alt)
+#         # print(EI_number_alt)
+#
+#         return EI_mass_alt, EI_number_alt
+#
+#     else:
+#         return None, None
+#     return None, None
 
 def p3t3_nvpm_meem(PT3_inflight, TT3_inflight, FAR_inflight, interp_func_far, interp_func_pt3, saf):
     """
@@ -623,7 +623,7 @@ def p3t3_nvpm_meem(PT3_inflight, TT3_inflight, FAR_inflight, interp_func_far, in
     ei_nvpm_mass = ei_nvpm_mass_sls * (PT3_inflight/pt3_sls)**1.35*(FAR_inflight/far_sls)**2.5
     # ei_nvpm_mass = ei_nvpm_mass_sls * (PT3_inflight / pt3_sls) ** 1.35 * (1.1) ** 2.5
 
-    result = ei_nvpm_mass * (ei_nvpm_number_sls/ ei_nvpm_mass_sls)
+    result = ei_nvpm_mass * (ei_nvpm_number_sls / ei_nvpm_mass_sls)
     print(result)
 
     return result
