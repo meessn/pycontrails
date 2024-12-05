c143590f32b663fe6f74a97a018eae6542b2e8fa
Mees Snoek <meessnoek@gmail.com>
2024-12-05 16:13:09 +0100

    fixed specific humidity in WAR calculation

--- water_injection_effect.py
+++ water_injection_effect.py
@@ -43,7 +43,7 @@ interp_func_pt3 = loaded_functions['interp_func_pt3']
 
 
 """FLIGHT PARAMETERS"""
-engine_model = 'GTF2035'        # GTF , GTF2035
+engine_model = 'GTF2035_wi_gass'        # GTF , GTF2035
 water_injection = [0, 0, 0]     # WAR climb cruise approach/descent
 SAF = 0                         # 0, 20, 100 unit = %
 flight = 'malaga'
