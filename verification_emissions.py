8062bc700de4a782ead4e136e47bd6866d73b8eb
Mees Snoek <meessnoek@gmail.com>
2024-12-07 19:22:43 +0100

    results midterm

--- verification_emissions.py
+++ verification_emissions.py
@@ -37,9 +37,9 @@ interp_func_pt3 = loaded_functions['interp_func_pt3']
 
 
 """FLIGHT PARAMETERS"""
-engine_model = 'GTF'        # GTF , GTF2035
-water_injection = [0, 0, 0]     # WAR climb cruise approach/descent
-SAF = 0                      # 0, 20, 100 unit = %
+engine_model = 'GTF2035_wi_gass_on_design'        # GTF , GTF2035
+water_injection = [10, 10, 10]     # WAR climb cruise approach/descent
+SAF = 0                     # 0, 20, 100 unit = %
 flight = 'malaga'
 aircraft = 'A20N_full'        # A20N ps model, A20N_wf is change in Thrust and t/o and idle fuel flows
                             # A20N_wf_opr is with changed nominal opr and bpr
