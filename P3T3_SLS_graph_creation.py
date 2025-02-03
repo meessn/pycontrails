373c90b8b6e765482d440f04199febe9a114a99b
Mees Snoek <meessnoek@gmail.com>
2025-02-03 17:36:09 +0100

    era5 download and p3t3 sls graph cfm5 (not final)

--- P3T3_SLS_graph_creation.py
+++ P3T3_SLS_graph_creation.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pickle
 
 # Load the CSV file (adjust the path to your CSV file)
-file_path = 'P3T3_SLS_GRAPHS_PW1127G_V2.csv'  # Replace with the actual file path
+file_path = 'P3T3_SLS_GRAPHS_CFM56.csv'  # Replace with the actual file path
 
 # Read the CSV file with the specified delimiter and decimal format
 data = pd.read_csv(file_path, delimiter=';', decimal=',')
@@ -28,7 +28,7 @@ interp_func_FAR = interp1d(TT3, FAR, kind='linear')
 interp_func_PT3 = interp1d(TT3, PT3, kind='linear')
 
 # Example usage: Get an interpolated FAR value for a specific TT3
-TT3_query = 600  # Replace with your desired TT3 value
+TT3_query = 450  # Replace with your desired TT3 value
 FAR_value = interp_func_FAR(TT3_query)
 PT3_value = interp_func_PT3(TT3_query)
 print(f"Interpolated FAR value at TT3={TT3_query}: {FAR_value}")
@@ -42,6 +42,6 @@ plt.show()
 
 functions = {'interp_func_far': interp_func_FAR, 'interp_func_pt3': interp_func_PT3}
 #save interpolation functions
-with open('p3t3_graphs_sls.pkl', 'wb') as f:
+with open('p3t3_graphs_sls_1990_2000.pkl', 'wb') as f:
     pickle.dump(functions, f)
 
--- era5_download.py
+++ era5_download.py
@@ -1,71 +1,215 @@
 import os
 import pandas as pd
 from pycontrails import Flight, MetDataset
-from pycontrails.datalib.ecmwf import ERA5
+from pycontrails.datalib.ecmwf import ERA5, ERA5ModelLevel
+from pycontrails.core.cache import DiskCacheStore
+from pathlib import Path
+import numpy as np
 from pycontrails.models.cocip import Cocip
 from pycontrails.datalib import ecmwf
 from datetime import datetime, timedelta
-# Path to the specific flight directory
-# lhr_ist_dir = "flight_trajectories/processed_flights/lhr_ist"
-directories = ["flight_trajectories/processed_flights/sin_maa"]
+# # Path to the specific flight directory
+# # lhr_ist_dir = "flight_trajectories/processed_flights/lhr_ist"
+# directories = ["flight_trajectories/processed_flights/sin_maa"]
+#
+# # Helper function to parse and adjust time bounds
+# def calculate_time_bounds(flight_data):
+#     """
+#     Calculate time bounds for meteorological data:
+#     - From the start time of the flight
+#     - To the end time of the flight + 12 hours
+#     """
+#     # Extract flight times
+#     start_time = flight_data["time"].min()
+#     end_time = flight_data["time"].max()
+#
+#
+#     # Adjust bounds
+#     adjusted_start = start_time
+#     adjusted_end = end_time + timedelta(hours=12)
+#
+#     return adjusted_start.strftime("%Y-%m-%d %H:%M"), adjusted_end.strftime("%Y-%m-%d %H:%M")
+#
+# for flight_dir in directories:
+#     print(f"Processing directory: {flight_dir}")
+#     # Loop through files in the lhr_ist directory
+#     for file in os.listdir(flight_dir):
+#         if file.endswith(".csv"):
+#             # Extract the full path to the flight file
+#             flight_path = os.path.join(flight_dir, file)
+#
+#             # Extract flight identifier (e.g., "lhr_ist_2023-02-06_daytime")
+#             flight_identifier = os.path.splitext(file)[0]
+#
+#             print(f"Processing flight: {flight_identifier}")
+#
+#             """------READ FLIGHT CSV AND PREPARE FORMAT---------------------------------------"""
+#             df = pd.read_csv(flight_path)  # Read the flight data
+#
+#             # Ensure the time column is parsed as datetime
+#             df["time"] = pd.to_datetime(df["time"])
+#
+#             fl = Flight(df)  # Initialize the flight object
+#
+#             """------CALCULATE TIME BOUNDS FOR METEOROLOGIC DATA-----------------------------"""
+#             time_bounds = calculate_time_bounds(df)  # Dynamically calculate time bounds
+#             print(f"Time bounds for {flight_identifier}: {time_bounds}")
+#
+#             """------RETRIEVE METEOROLOGIC DATA----------------------------------------------"""
+#             pressure_levels = (
+#             1000, 950, 900, 850, 800, 750, 700, 650, 600, 550, 500, 450, 400, 350, 300, 250, 225, 200, 175)  # hPa
+#
+#             era5pl = ERA5(
+#                 time=time_bounds,
+#                 variables=Cocip.met_variables + Cocip.optional_met_variables + (ecmwf.PotentialVorticity,) + (
+#                 ecmwf.RelativeHumidity,),
+#                 pressure_levels=pressure_levels,
+#             )
+#             era5sl = ERA5(time=time_bounds, variables=Cocip.rad_variables + (ecmwf.SurfaceSolarDownwardRadiation,))
+#
+#             # Download data from ERA5 (or open from cache)
+#             met = era5pl.open_metdataset()  # Meteorology
+#             rad = era5sl.open_metdataset()  # Radiation
+#
+#             print(f"Finished processing flight: {flight_identifier}")
 
-# Helper function to parse and adjust time bounds
-def calculate_time_bounds(flight_data):
-    """
-    Calculate time bounds for meteorological data:
-    - From the start time of the flight
-    - To the end time of the flight + 12 hours
-    """
-    # Extract flight times
-    start_time = flight_data["time"].min()
-    end_time = flight_data["time"].max()
+# time_bounds = ("2023-02-05 14:00", "2023-02-07 11:00")
+# time_bounds = ("2023-05-04 14:00", "2023-05-06 11:00")
+# time_bounds = ("2023-08-05 14:00", "2023-08-07 11:00")
+# time_bounds = ("2023-11-05 14:00", "2023-08-07 11:00")
 
+"""FLIGHT PARAMETERS"""
+engine_model = 'GTF'        # GTF , GTF2035, GTF1990, GTF2000
+water_injection = [0, 0, 0]     # WAR climb cruise approach/descent
+SAF = 0                   # 0, 20, 100 unit = %
+flight = 'malaga'
+aircraft = 'A20N_full'        # A20N ps model, A20N_wf is change in Thrust and t/o and idle fuel flows
+                            # A20N_wf_opr is with changed nominal opr and bpr
+                            # A20N_full has also the eta 1 and 2 and psi_0
 
-    # Adjust bounds
-    adjusted_start = start_time
-    adjusted_end = end_time + timedelta(hours=12)
+"""------READ FLIGHT CSV AND PREPARE FORMAT---------------------------------------"""
+df = pd.read_csv(f"{flight}_flight.csv")
+df = df.rename(columns={'geoaltitude': 'altitude', 'groundspeed': 'groundspeed', 'timestamp':'time'})
+df = df.dropna(subset=['callsign'])
+df = df.dropna(subset=['altitude'])
+df = df.drop(['Unnamed: 0', 'icao24', 'callsign'], axis=1)
 
-    return adjusted_start.strftime("%Y-%m-%d %H:%M"), adjusted_end.strftime("%Y-%m-%d %H:%M")
+# df = df[df['altitude'] > 1900]
+column_order = ['longitude', 'latitude', 'altitude', 'groundspeed', 'time']
+df = df[column_order]
+df['altitude'] = df['altitude']*0.3048 #foot to meters
+df['groundspeed'] = df['groundspeed']*0.514444444
 
-for flight_dir in directories:
-    print(f"Processing directory: {flight_dir}")
-    # Loop through files in the lhr_ist directory
-    for file in os.listdir(flight_dir):
-        if file.endswith(".csv"):
-            # Extract the full path to the flight file
-            flight_path = os.path.join(flight_dir, file)
+if engine_model == 'GTF' or engine_model == 'GTF2035':
+    engine_uid = '01P22PW163'
+elif engine_model == 'GTF1990':
+    engine_uid = '1CM009'
+elif engine_model == 'GTF2000':
+    engine_uid = '3CM026'
+else:
+    raise ValueError(f"Unsupported engine_model: {engine_model}. ")
 
-            # Extract flight identifier (e.g., "lhr_ist_2023-02-06_daytime")
-            flight_identifier = os.path.splitext(file)[0]
+attrs = {
+    "flight_id" : "34610D",
+    "aircraft_type": f"{aircraft}",
+    "engine_uid": f"{engine_uid}"
+}
 
-            print(f"Processing flight: {flight_identifier}")
+fl = Flight(df, attrs=attrs)
 
-            """------READ FLIGHT CSV AND PREPARE FORMAT---------------------------------------"""
-            df = pd.read_csv(flight_path)  # Read the flight data
+"""------ERA5model--------------------------------"""
+# time_bounds_list = [
+#     ("2023-02-05 14:00", "2023-02-07 11:00"),
+#     ("2023-05-04 14:00", "2023-05-06 11:00"),
+#     ("2023-08-05 14:00", "2023-08-07 11:00"),
+#     ("2023-11-05 14:00", "2023-11-07 11:00")  # Fixed the incorrect end date
+# ]
+#
+# time_step = timedelta(hours=6)  # Subdivide into 6-hour chunks
+#
+# pressure_levels_10 = np.arange(150, 400, 10)  # 150 to 400 with steps of 10
+# pressure_levels_50 = np.arange(400, 1001, 50)  # 400 to 1000 with steps of 50
+# pressure_levels_model = np.concatenate((pressure_levels_10, pressure_levels_50))
+#
+# local_cache_dir = Path("F:/era5model/flights")
+# local_cachestore = DiskCacheStore(cache_dir=local_cache_dir)
+#
+# for start_time_str, end_time_str in time_bounds_list:
+#     start_time = datetime.strptime(start_time_str, "%Y-%m-%d %H:%M")
+#     end_time = datetime.strptime(end_time_str, "%Y-%m-%d %H:%M")
+#
+#     current_time = start_time
+#     while current_time < end_time:
+#         next_time = min(current_time + time_step, end_time)
+#         time_bounds = (current_time.strftime("%Y-%m-%d %H:%M"), next_time.strftime("%Y-%m-%d %H:%M"))
+#         print(f"Processing time bounds: {time_bounds}")
+#
+#         try:
+#             era5ml = ERA5ModelLevel(
+#                 time=time_bounds,
+#                 variables=("t", "q", "u", "v", "w", "ciwc"),
+#                 model_levels=range(67, 133),
+#                 pressure_levels=pressure_levels_model,
+#                 cachestore=local_cachestore
+#             )
+#
+#             met = era5ml.open_metdataset()
+#             print(f"Successfully processed {time_bounds}")
+#
+#         except Exception as e:
+#             print(f"Error processing {time_bounds}: {e}")
+#
+#         # Move to the next time chunk
+#         current_time = next_time
+#
+# print("All processing complete.")
 
-            # Ensure the time column is parsed as datetime
-            df["time"] = pd.to_datetime(df["time"])
+"""ERA5"""
 
-            fl = Flight(df)  # Initialize the flight object
+time_bounds_list = [
+    ("2023-02-05 14:00", "2023-02-07 11:00"),
+    ("2023-05-04 14:00", "2023-05-06 11:00"),
+    ("2023-08-05 14:00", "2023-08-07 11:00"),
+    ("2023-11-05 14:00", "2023-11-07 11:00")  # Fixed the incorrect end date
+]
 
-            """------CALCULATE TIME BOUNDS FOR METEOROLOGIC DATA-----------------------------"""
-            time_bounds = calculate_time_bounds(df)  # Dynamically calculate time bounds
-            print(f"Time bounds for {flight_identifier}: {time_bounds}")
+time_step = timedelta(hours=6)  # Subdivide into 6-hour chunks
 
-            """------RETRIEVE METEOROLOGIC DATA----------------------------------------------"""
-            pressure_levels = (
+pressure_levels = (
             1000, 950, 900, 850, 800, 750, 700, 650, 600, 550, 500, 450, 400, 350, 300, 250, 225, 200, 175)  # hPa
 
+local_cache_dir = Path("F:/era5pressure/Cache")
+local_cachestore = DiskCacheStore(cache_dir=local_cache_dir)
+
+for start_time_str, end_time_str in time_bounds_list:
+    start_time = datetime.strptime(start_time_str, "%Y-%m-%d %H:%M")
+    end_time = datetime.strptime(end_time_str, "%Y-%m-%d %H:%M")
+
+    current_time = start_time
+    while current_time < end_time:
+        next_time = min(current_time + time_step, end_time)
+        time_bounds = (current_time.strftime("%Y-%m-%d %H:%M"), next_time.strftime("%Y-%m-%d %H:%M"))
+        print(f"Processing time bounds: {time_bounds}")
+
+        try:
             era5pl = ERA5(
-                time=time_bounds,
-                variables=Cocip.met_variables + Cocip.optional_met_variables + (ecmwf.PotentialVorticity,) + (
-                ecmwf.RelativeHumidity,),
-                pressure_levels=pressure_levels,
-            )
-            era5sl = ERA5(time=time_bounds, variables=Cocip.rad_variables + (ecmwf.SurfaceSolarDownwardRadiation,))
+                            time=time_bounds,
+                            variables=Cocip.met_variables + Cocip.optional_met_variables + (ecmwf.PotentialVorticity,) + (
+                            ecmwf.RelativeHumidity,),
+                            pressure_levels=pressure_levels,
+                            cachestore=local_cachestore
+                        )
+            era5sl = ERA5(time=time_bounds, variables=Cocip.rad_variables + (ecmwf.SurfaceSolarDownwardRadiation,), cachestore=local_cachestore)
 
             # Download data from ERA5 (or open from cache)
             met = era5pl.open_metdataset()  # Meteorology
             rad = era5sl.open_metdataset()  # Radiation
 
-            print(f"Finished processing flight: {flight_identifier}")
+        except Exception as e:
+            print(f"Error processing {time_bounds}: {e}")
+
+        # Move to the next time chunk
+        current_time = next_time
+
+print("All processing complete.")
+
